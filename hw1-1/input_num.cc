46bf354ea24d4e489dcd75e71a104dd8334f440f
sohanmin <world2013@naver.com>
2020-09-12 21:12:27 +0900

    backup

---
+++ hw1-1/input_num.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    int num;
+    std::cin >> num;
+    std::cout << num << std::endl;
+    return 0;
+}
---
+++ hw1-1/main.cc
@@ -0,0 +1,6 @@
+void print_hello();
+
+int main(){
+    print_hello();
+    return 0;
+}
